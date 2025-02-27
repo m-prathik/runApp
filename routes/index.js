3d2e4af4573080b29df74786668797ec327e73db
LOKERAY <prathikmanchikalapudi7@gmail.com>
2025-02-27 20:04:46 +0530

    changes for 3rd commit

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var prettier = require('prettier');
 
-const userTable = require('../db/model/userModel.js');
+const data = require('../db/model/userModel.js');
 var router = express.Router();
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
 router.get('/allUsers',async function(req, res, next){
-    const result = await userTable.find();
+    const result = await data.userTable.find();
     res.send(JSON.stringify(result, null));
 })
 
--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,18 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 const { userExistInDb } = require('../db/model/userModel.js');
-
+router.use(express.json());
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 router.post('/addUser', async function(req, res, next) {
+  console.log("/addUser api called");
+  console.log(req.body);
   var value = req.body.userName;
   if(await userExistInDb(value)){
+    console.log("reached here");
     res.send("alreadyExists");
   }
   else {
